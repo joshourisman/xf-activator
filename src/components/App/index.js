62c6b86277a40ab2407c45832394507420ed878f
Josh Ourisman <josh@joshourisman.com>
2017-09-16 15:42:42 -0400

    Converted to single-page app.

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,32 +1,13 @@
-import React, { PropTypes } from 'react';
-import Link from 'react-router/lib/Link';
+import React from 'react';
+
 import styles from './styles.scss';
 
-function App({ children }) {
-  return (
-    <div>
-      <i className={styles.logo} />
-      <ul className={styles.nav}>
-        <li className={styles.navItem}>
-          <Link className={styles.link} to="/">
-            Home
-          </Link>
-        </li>
-        <li className={styles.navItem}>
-          <Link className={styles.link} to="/tools">
-            Tools
-          </Link>
-        </li>
-      </ul>
-      <div className={styles.content}>
-        {children}
-      </div>
+const App = () =>
+  <div>
+    <i className={styles.logo} />
+    <div className={styles.content}>
+      <h1>Hi!</h1>
     </div>
-  );
-}
-
-App.propTypes = {
-  children: PropTypes.node.isRequired,
-};
+  </div>;
 
 export default App;
--- src/server/index.js
+++ src/server/index.js
@@ -3,11 +3,8 @@ import compression from 'compression';
 import path from 'path';
 import React from 'react';
 import { renderToString } from 'react-dom/server';
-import RouterContext from 'react-router/lib/RouterContext';
-import createMemoryHistory from 'react-router/lib/createMemoryHistory';
-import match from 'react-router/lib/match';
 import template from './template';
-import routes from '../routes';
+import App from '../components/App';
 
 const clientAssets = require(KYT.ASSETS_MANIFEST); // eslint-disable-line import/no-dynamic-require
 const port = parseInt(KYT.SERVER_PORT, 10);
@@ -24,29 +21,15 @@ app.use(express.static(path.join(process.cwd(), KYT.PUBLIC_DIR)));
 
 // Setup server side routing.
 app.get('*', (request, response) => {
-  const history = createMemoryHistory(request.originalUrl);
-
-  match({ routes, history }, (error, redirectLocation, renderProps) => {
-    if (error) {
-      response.status(500).send(error.message);
-    } else if (redirectLocation) {
-      response.redirect(302, `${redirectLocation.pathname}${redirectLocation.search}`);
-    } else if (renderProps) {
-      // When a React Router route is matched then we render
-      // the components and assets into the template.
-      response.status(200).send(
-        template({
-          root: renderToString(<RouterContext {...renderProps} />),
-          manifestJSBundle: clientAssets['manifest.js'],
-          mainJSBundle: clientAssets['main.js'],
-          vendorJSBundle: clientAssets['vendor.js'],
-          mainCSSBundle: clientAssets['main.css'],
-        })
-      );
-    } else {
-      response.status(404).send('Not found');
-    }
-  });
+  response.status(200).send(
+    template({
+      root: renderToString(<App />),
+      manifestJSBundle: clientAssets['manifest.js'],
+      mainJSBundle: clientAssets['main.js'],
+      vendorJSBundle: clientAssets['vendor.js'],
+      mainCSSBundle: clientAssets['main.css'],
+    })
+  );
 });
 
 app.listen(port, () => {
