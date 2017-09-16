0e98c48acf317bdba2d598238786ce4b726cdc20
Josh Ourisman <josh@joshourisman.com>
2017-09-16 15:43:58 -0400

    Removed all SASS stuff.

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,11 +1,8 @@
 import React from 'react';
 
-import styles from './styles.scss';
-
 const App = () =>
   <div>
-    <i className={styles.logo} />
-    <div className={styles.content}>
+    <div>
       <h1>Hi!</h1>
     </div>
   </div>;
